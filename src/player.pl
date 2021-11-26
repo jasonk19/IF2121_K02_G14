4d6f36aa6d9087aeefe2d7d464d47dc3cc7083dd
NayotamaPradipta <n4y0t4m4@gmail.com>
2021-11-27 02:06:35 +0700

    Tambah status, growthrate, entrystat di player

--- src/player.pl
+++ src/player.pl
@@ -1,3 +1,22 @@
+:- dynamic(exp/3).
+:- dynamic(player/10).
+:- dynamic(equipment/2).
+/* Job, specific job level, specific job exp */
+/* Kalo semisal terlalu kecil atau terlalu besar bisa diubah nantinya */
+growthRate(farmer, 1, 20).
+growthRate(fisherman, 1, 30).
+growthRate(rancher, 1, 20).
+
+/* Entrystat saat masih level 1*/
+/* Format: Job, specific job level, specific job exp, exp, gold */
+entryStat(farmer, 1, 56, 0, 1000).
+entryStat(fisherman, 1, 76, 0, 1000).
+entryStat(rancher, 1, 56, 0, 1000).
+
+entryExp :- 
+  retractall(exp(_,_,_)),
+  assertz(exp(0,300,1)).
+
 choose_job :- 
   write('Welcome to Harvest. Choose your job'), nl,
   write('1. Fisherman'), nl,
@@ -18,6 +37,21 @@ character(ID) :-
 character(ID) :-
   ID =:= 2 -> write('You choose Rancher, let\'s start ranching').
   /* Add Rancher status */
+
+/* Add print Status */ 
+status :- player(Job, Level, FarmLevel, FarmExp, FishLevel, FishExp, RanchLevel, RanchExp, Exp, Gold),
+            equipment(Farming, Fishing),
+            write('Your status: '), nl, 
+            write('Job: '), write(Job), nl,
+            write('Level: '), write(Level), nl,
+            write('Level farming: '), write(FarmLevel), nl,
+            write('Exp farming: '), write(FarmExp), nl,
+            write('Level fishing: '), write(FishLevel), nl,
+            write('Exp fishing: '), write(FishExp), nl,
+            write('Level ranching: '), write(RanchLevel), nl,
+            write('Exp ranching: '), write(RanchExp), nl,
+            write('Exp: '), write(Exp), , write('/'), exp(_,Max,_), write(Max), nl,
+            write('Gold: '), write(Gold), nl.
 
 /* Format Job */
 job(0, 'Fisherman').
