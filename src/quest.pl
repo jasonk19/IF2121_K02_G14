fb0a353281cc7a7b34e70a95efb16315d1d048cb
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2021-11-27 19:04:13 +0700

    add fulfillQuest

--- src/quest.pl
+++ src/quest.pl
@@ -60,7 +60,7 @@ getQuest :-
         ;
             random(0,2,Egg_type),
             (Egg_type == 0 ->
-                Ranch_id is 30, 
+                Ranch_id is 30,
                 write('- '),write(Ranch),write(' egg'),nl
             ;
                 Ranch_id is 31, 
@@ -90,19 +90,29 @@ displayQuest :-
     ;
         !).
 
-
+/* Finishing Quest */
 fulfillQuest :-
     quest(Id_Harvest, Qty_Harvest, Id_Fish, Qty_Fish, Id_Ranch, Qty_Ranch),
-    inventory(Id_Harvest,Name_Harvest,Qty_H,_,_,_,_,_),
-    inventory(Id_Fish,Name_Fish,Qty_F,_,_,_,_,_),
-    inventory(Id_Ranch,Name_Ranch,Qty_R,_,_,_,_,_),
+    inventory(Id_Harvest,Name_Harvest,Qty_H,Sell_H,_,_,_,_),
+    inventory(Id_Fish,Name_Fish,Qty_F,Sell_F,_,_,_,_),
+    inventory(Id_Ranch,Name_Ranch,Qty_R,Sell_R,_,_,_,_),
     ((Qty_H >= Qty_Harvest, Qty_F >= Qty_Fish, Qty_R >= Qty_Ranch) ->
         displayQuest, write('Finish current quest?(y/n)'), read(X),
         (X == 'y' ->
+            Reward_Harvest is Qty_H * Sell_H * 2,
+            Reward_Fish is Qty_F * Sell_F * 2,
+            Reward_Ranch is Qty_R * Sell_R * 2,
+            Reward_Gold is Reward_Fish + Reward_Harvest + Reward_Ranch,
+            Reward_Exp is 500,
+	    write('Quest finished, you gain:),nl,write(Reward_Gold),write(' gold and '),write(Reward_Exp),write(' Exp'),
+            player(Job, Level, FarmLevel, FarmExp, FishLevel, FishExp, RanchLevel, RanchExp, Exp, Gold),
+            Total_Gold is Reward_Gold + Gold,
+            Total_Exp is Reward_Exp + Exp,
+            retract(player(Job, Level, FarmLevel, FarmExp, FishLevel, FishExp, RanchLevel, RanchExp, Exp, Gold)),
+            asserta(player(Job, Level, FarmLevel, FarmExp, FishLevel, FishExp, RanchLevel, RanchExp, Total_Exp, Total_Gold))
             delItems(Name_Harvest,Qty_H),
             delItems(Name_Fish,Qty_F),
             delItems(Name_Ranch,Qty_R),
-            /* get exp and money */
             retract(ongoing),
 	        retract(quest(Id_Harvest, Qty_Harvest, Id_Fish, Qty_Fish, Id_Ranch, Qty_Ranch))
         ;
@@ -116,8 +126,7 @@ quest :-
     map_quest(Q),map_object(XQ,YQ,Q),map_player(P),map_object(XP,YP,P),
     ((XQ =:= XP, YQ =:= YP) ->
         (ongoing ->
-            /*fulfillQuest*/
-            write('You have an on-going quest!'),!
+            fulfillQuest,!
         ;
             getQuest,
             assertz(ongoing),!
