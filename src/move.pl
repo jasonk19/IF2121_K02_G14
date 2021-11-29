1a0d85f31c60bb1b3957931ddca779473fdab7df
jask-19 <16520214@std.stei.itb.ac.id>
2021-11-29 12:51:38 +0700

    Minor fix in move

--- src/move.pl
+++ src/move.pl
@@ -8,11 +8,6 @@ w :- inHouse, !, write('You are at your House, please exit your House to move').
 w :- inMarket, !, write('You are at the Market, please exit the Market to move').
 w :- inRanch, !, write('You are at your Ranch, please exit your Ranch to move').
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 32407356823761f325a107ada02c8398eae18def
 w :-
   planted(_,_,_,Time),
   (Time =:= 0 -> (!) ; NTime is Time - 1, retract(planted(_,_,_,Time)), assertz(planted(_,_,_,NTime)), !).
@@ -45,10 +40,6 @@ s :- inHouse, !, write('You are at your House, please exit your House to move').
 s :- inMarket, !, write('You are at the Market, please exit the Market to move').
 s :- inRanch, !, write('You are at your Ranch, please exit your Ranch to move').
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 32407356823761f325a107ada02c8398eae18def
 s :-
   planted(_,_,_,Time),
   (Time =:= 0 -> (!) ; NTime is Time - 1, retract(planted(_,_,_,Time)), assertz(planted(_,_,_,NTime)), !).
@@ -80,10 +71,6 @@ a :- inHouse, !, write('You are at your House, please exit your House to move').
 a :- inMarket, !, write('You are at the Market, please exit the Market to move').
 a :- inRanch, !, write('You are at your Ranch, please exit your Ranch to move').
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 32407356823761f325a107ada02c8398eae18def
 a :-
   planted(_,_,_,Time),
   (Time =:= 0 -> (!) ; NTime is Time - 1, retract(planted(_,_,_,Time)), assertz(planted(_,_,_,NTime)), !).
