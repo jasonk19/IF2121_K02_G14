66ba9bfafc5263637d585fd7a2960b378af7a29e
kentlius <kent.delatinos@gmail.com>
2021-11-29 13:07:29 +0700

    validasi rod fishing

--- src/fishing.pl
+++ src/fishing.pl
@@ -3,7 +3,7 @@
 :- include('map.pl').
 
 /* Buat testing */
-map_object(2,11,'P',_).
+map_object(1,11,'P',_).
 map_object(3,11,'o',_).
 map_object(4,11,'o',_).
 map_object(5,11,'o',_).
@@ -12,34 +12,62 @@ map_object(3,12,'o',_).
 map_object(4,12,'o',_).
 map_object(5,12,'o',_).
 
+
 /* Level Fishing Sementara */
 
 
-/*  rod blm */
-
 fish :- 
-	isNearObject('o'),!,
+	isNearObject('o'), inventory(_,wooden_fishing_rod,_,_,_,_,_,_),
 	player(_, _, _, _, FishLevel, _, _, _, _, _),
 	( FishLevel < 5
 	-> random(1, 4, FishID), random(0,6,Qty)
 	
-	; ( FishLevel < 10 
-	  -> random(1, 7, FishID), random(0,6,Qty)
-	  
-	  ; ( FishLevel < 15
-	    -> random(1, 10, FishID), random(0,6,Qty)
-		
-		; random(1, 11, FishID), random(0,6,Qty)
-			
-		)
-	  )
-	),
+	; 	( FishLevel < 10 
+		-> random(1, 7, FishID), random(0,6,Qty)
 	
+		; 	( FishLevel < 15
+			-> random(1, 10, FishID), random(0,6,Qty)
+		
+			;	random(1, 11, FishID), random(0,6,Qty)
+			
+			)
+		)
+	),
+
 	( Qty =:= 0
 	-> write('You didnt get anything!'), nl,
 	addFishExp(5),!
 	
 	; addItems(FishID,Qty),nl,
-	addFishExp(10),!
+	TotalXP is Qty * 5,
+	addFishExp(5 + TotalXP),!
+	
+	).
+
+fish :- 
+	isNearObject('o'), inventory(_,stone_fishing_rod,_,_,_,_,_,_),
+	player(_, _, _, _, FishLevel, _, _, _, _, _),
+	( FishLevel < 5
+	-> random(1, 4, FishID), random(0,11,Qty)
+	
+	; 	( FishLevel < 10 
+		-> random(1, 7, FishID), random(0,11,Qty)
+	
+		; 	( FishLevel < 15
+			-> random(1, 10, FishID), random(0,11,Qty)
+		
+			;	random(1, 11, FishID), random(0,11,Qty)
+			
+			)
+		)
+	),
+
+	( Qty =:= 0
+	-> write('You didnt get anything!'), nl,
+	addFishExp(5),!
+	
+	; addItems(FishID,Qty),nl,
+	TotalXP is Qty * 5,
+	addFishExp(5 + TotalXP),!
 	
 	).
