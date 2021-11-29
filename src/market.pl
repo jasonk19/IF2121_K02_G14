c31fa3960bb631b3f92289ec3266f05012c12a02
kentlius <kent.delatinos@gmail.com>
2021-11-29 12:18:28 +0700

    update gold market

--- src/market.pl
+++ src/market.pl
@@ -1,12 +1,10 @@
 :- include('inventory.pl').
 :- include('map.pl').
+:- include('player.pl').
 
 /* Buat testing */
 map_object(2,3,'P',_).
 map_object(2,3,'M',_).
-
-/* Gold player */
-player(_, _, _, _, _, _, _, _, _, 149).
 
 market :-
     map_player(P), map_object(X,Y,P,_),
@@ -20,18 +18,19 @@ market :-
     ; write('You are not at the Market!'), !).
 
 displayMarket :- 
+    player(_, _, _, _, _, _, _, _, _, Gold),
+    format('Current Gold: ~w ~n',[Gold]),
     write('What do you want to buy?'), nl,
-	write('1. Carrot seed (50 golds)'), nl,
+	write('1. Carrot seed (30 golds)'), nl,
 	write('2. Corn seed (50 golds)'), nl,
-	write('3. Tomato seed (50 golds)'), nl,
-	write('4. Potato seed (50 golds)'), nl,
+	write('3. Tomato seed (100 golds)'), nl,
+	write('4. Potato seed (60 golds)'), nl,
 	write('5. Chicken (500 golds)'), nl,
-	write('6. Sheep (1000 golds)'), nl,
-	write('7. Cow (1500 golds)'), nl,
-	write('8. Level 2 shovel (300 golds)'), nl,
-	write('9. Level 2 fishing rod (500 golds)'), nl.
-
-/* blom ngurangin duit */
+	write('6. Sheep (700 golds)'), nl,
+	write('7. Cow (1000 golds)'), nl,
+	write('8. Shovel (200 golds)'), nl,
+	write('9. Wooden fishing rod (200 golds)'), nl,
+    write('10. Stone fishing rod (300 golds)'), nl.
 
 buy :-
     (inMarket 
@@ -40,49 +39,49 @@ buy :-
         ( Num =:= 1
             -> write('How many do you want to buy?'),nl,
             write('> '), read(BuyQty), nl,
-            items(_,carrot,_,_,Price,_,_,_),
-            player(_, _, _, _, _, _, _, _, _, Gold),
+            items(_,carrot_seed,_,_,Price,_,_,_),
+            player(_, _, _, _, _, _, _, _, _, _, Gold),
             Total is BuyQty * Price,
             ( Total =< Gold
-                -> addItems(carrot,BuyQty),nl,
+                -> addItems(carrot_seed,BuyQty),nl,
                 format('You have bought ~w Carrot seed.', [BuyQty]),nl,
-                format('You are charged ~w golds.', [Total]),nl
+                reduceGold(Total),nl
                 ; write('Not enough golds'),nl)
         
             ; Num =:= 2
                 -> write('How many do you want to buy?'),nl,
                 write('> '), read(BuyQty), nl,
-                items(_,corn,_,_,Price,_,_,_),
+                items(_,corn_seed,_,_,Price,_,_,_),
                 player(_, _, _, _, _, _, _, _, _, Gold),
                 Total is BuyQty * Price,
                 ( Total =< Gold
-                    -> addItems(corn,BuyQty),nl,
+                    -> addItems(corn_seed,BuyQty),nl,
                     format('You have bought ~w Corn seed.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 3
                 -> write('How many do you want to buy?'),nl,
                 write('> '), read(BuyQty), nl,
-                items(_,tomato,_,_,Price,_,_,_),
+                items(_,tomato_seed,_,_,Price,_,_,_),
                 player(_, _, _, _, _, _, _, _, _, Gold),
                 Total is BuyQty * Price,
                 ( Total =< Gold
-                    -> addItems(tomato,BuyQty),nl, 
+                    -> addItems(tomato_seed,BuyQty),nl, 
                     format('You have bought ~w Tomato seed.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 4
                 -> write('How many do you want to buy?'),nl,
                 write('> '), read(BuyQty), nl,
-                items(_,potato,_,_,Price,_,_,_),
+                items(_,potato_seed,_,_,Price,_,_,_),
                 player(_, _, _, _, _, _, _, _, _, Gold),
                 Total is BuyQty * Price,
                 ( Total =< Gold
-                    -> addItems(potato,BuyQty),nl,
+                    -> addItems(potato_seed,BuyQty),nl,
                     format('You have bought ~w Potato seed.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 5
@@ -94,7 +93,7 @@ buy :-
                 ( Total =< Gold
                     -> addItems(chicken,BuyQty),nl,
                     format('You have bought ~w Chicken.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 6
@@ -106,7 +105,7 @@ buy :-
                 ( Total =< Gold
                     -> addItems(sheep,BuyQty),nl,
                     format('You have bought ~w Sheep.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 7
@@ -118,7 +117,7 @@ buy :-
                 ( Total =< Gold
                     -> addItems(cow,BuyQty),nl,
                     format('You have bought ~w Cow.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
                 
             ; Num =:= 8
@@ -130,7 +129,7 @@ buy :-
                 ( Total =< Gold
                     -> addItems(shovel,BuyQty),nl,
                     format('You have bought ~w Shovel.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
+                    reduceGold(Total),nl
                     ; write('Not enough golds'),nl)
         
             ; Num =:= 9
@@ -141,13 +140,23 @@ buy :-
                 Total is BuyQty * Price,
                 ( Total =< Gold
                     -> addItems(wooden_fishing_rod,BuyQty),nl,
-                    format('You have bought ~w Fishing Rod.', [BuyQty]),nl,
-                    format('You are charged ~w golds.', [Total]),nl
-                    ; write('Not enough golds'),nl))
+                    format('You have bought ~w Wooden Fishing Rod.', [BuyQty]),nl,
+                    reduceGold(Total),nl
+                    ; write('Not enough golds'),nl)
+
+            ; Num =:= 10
+            -> write('How many do you want to buy?'),nl,
+            write('> '), read(BuyQty), nl,
+            items(_,stone_fishing_rod,_,_,Price,_,_,_),
+            player(_, _, _, _, _, _, _, _, _, Gold),
+            Total is BuyQty * Price,
+            ( Total =< Gold
+                -> addItems(stone_fishing_rod,BuyQty),nl,
+                format('You have bought ~w Wooden Fishing Rod.', [BuyQty]),nl,
+                reduceGold(Total),nl
+                ; write('Not enough golds'),nl))
 
         ; write('You are not at the Market!')).
-
-/* blom nambah duit */
 
 sell :- 
     (inMarket
@@ -161,7 +170,8 @@ sell :-
             -> format('You dont have enough ~w. Cancelling...', [ItemName])
             ; delItems(ItemName, SellQty), 
             Total is SellPrice * SellQty,
-            format('You sold ~w ~w for ~w Golds.', [SellQty, ItemName, Total]))
+            format('You sold ~w ~w ~n.', [SellQty, ItemName]),
+            addGold(Total))
 
         ; write('You are not at the Market!')).
 
